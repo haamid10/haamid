12cdd2768d50fd172536cac2909e1526712e53de
haamid10 <apdixamiid919@gmail.com>
2023-04-19 05:36:16 +0300

    setting up the invirament

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import Index from './Components/Index'
 const App = () => {
   return (
-    <div className='bg-gradient-to-r from-[#060206] to-[#0B1926] h-screen w-full'>
+    <div className='bg-gradient-to-r from-[#060206] to-[#0B1926] h-screen w-full text-white'>
       <div>
         <h1 className='text-white text-6xl font-bold'>suii</h1>
+        <Index/>
       </div>
     </div>
   )
