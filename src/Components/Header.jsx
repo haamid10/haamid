276163943435ee72b278c8be6f0a9800e39f028f
haamid10 <apdixamiid919@gmail.com>
2023-04-19 17:52:17 +0300

    addeed

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,15 +4,23 @@ import CV from '../assets/intern.pdf'
 const Header = () => {
   return (
    <header>
-    <div>
-      <h5>Hello I'm</h5>
-      <h1>Hamiid apdi</h1>
-      <h5>Fullstack Developer</h5>
+    <div className=' flex flex-col items-center justify-start '>
+      <div>
+
+      
+      <h5 className=' font-semibold mb-3'>Hello I'm</h5>
+      <h1 className='font-bold text-4xl mb-2'>Hamiid apdi</h1>
+      <h5 className='font-light mb-2'>Fullstack Developer</h5>
+      </div>
+
+    <div className='mr-12  gap-8 flex'>
+      <a className=' border-2 border-green px-2 py-2 rounded-md  hover:bg-green text-sm text-center  font-semibold hover:text-black hover:border-none
+      ' href={CV} view>View CV</a>
+      <a className='  bg-green px-2 py-2 text-black rounded-md   font-semibold hover:text-white hover:bg-transparent hover:border-2 hover:border-green text-sm
+      ' href='' view>Let's Talk</a>
+    </div>
     </div>
 
-    <div>
-      <a href={CV} view>Download</a>
-    </div>
 
    </header>
   )
