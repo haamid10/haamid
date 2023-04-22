69cb8c494cb6d7c8915ac48c1d8ba15c1fe7794b
haamid10 <apdixamiid919@gmail.com>
2023-04-22 18:14:05 +0300

    d

--- src/Components/headerDive/Header.jsx
+++ src/Components/headerDive/Header.jsx
@@ -9,18 +9,18 @@ const Header = () => {
       <div className='sm:mb-4 lg:mb-12'>
         <h5 className=' font-semibold lg:text-sm mb-3'>Hello I'm</h5>
         <h1 className='font-bold text-2xl lg:text-4xl mb-2'>Hamiid apdi</h1>
-        <h5 className='font-light mb-2 lg:text-xl text-sm'>Fullstack Developer</h5>
+        <h5 className='font-light mb-2 lg:text-xl text-sm'>mern-stack Developer</h5>
         <div className='mr-12 gap-8  flex'>
         <a className=' border border-greeen px-2 py-2 rounded-md  hover:bg-greeen text-sm text-center  font-semibold hover:text-black hover:border-none
         ' href={CV} view>View CV</a>
         <a className='  bg-greeen p-2 text-center text-black rounded-md   font-semibold hover:text-white hover:bg-transparent hover:border hover:border-greeen text-sm
         ' href='' view>Let's Talk</a>
       </div>
-      </div>
+      </div>d
       <HeaderSocials className=""/>
 
-<div>
-<div className='absolute top-72  me'>
+<div className=' mt-0 mr-8'>
+<div className='  me'>
       </div>
       <img src={Me} className='  m3 ' alt="Haamid" />
 
