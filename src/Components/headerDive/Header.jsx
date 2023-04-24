fe3c1bdbbd083d3a754fe3c97ea378e7285d2089
haamid10 <apdixamiid919@gmail.com>
2023-04-24 22:07:02 +0300

    clubs is best place

--- src/Components/headerDive/Header.jsx
+++ src/Components/headerDive/Header.jsx
@@ -10,10 +10,10 @@ const Header = () => {
         <h5 className=' font-semibold lg:text-sm mb-3'>Hello I'm</h5>
         <h1 className='font-bold text-2xl lg:text-4xl mb-2'>Hamiid apdi</h1>
         <h5 className='font-light mb-2  '>mern-stack Developer</h5>
-        <div className='mr-12 gap-8  flex'>
-        <a className=' border border-greeen px-2 py-2 rounded-md  hover:bg-greeen text-sm text-center  font-semibold hover:text-black hover:border-none
+        <div className='mr-12 gap-2 lg:gap-8  flex'>
+        <a className=' border w-24 lg:w-fit lg:h-fit h-8 py-1 lg:py-2 border-greeen  lg:px-2 rounded-md  hover:bg-greeen text-sm text-center  font-semibold hover:text-black hover:border-none
         ' href={CV} view>View CV</a>
-        <a className='  bg-greeen p-2 text-center text-black rounded-md   font-semibold hover:text-white hover:bg-transparent hover:border hover:border-greeen text-sm
+        <a className='w-24 lg:w-fit lg:h-fit h-8 py-1 lg:py-2  bg-greeen p-2 text-center text-black rounded-md   font-semibold hover:text-white hover:bg-transparent hover:border hover:border-greeen text-sm
         ' href='' view>Let's Talk</a>
       </div>
       </div>
