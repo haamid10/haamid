1ca10b906dcd8885dd8aed0f52f19e3b4f3f1dd7
haamid10 <apdixamiid919@gmail.com>
2023-04-30 16:50:37 +0300

    done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -11,8 +11,8 @@ const Navbar = () => {
   return (
     <div>
       Navbar
-      <nav className=' flex  w-fit p-5 gap-6 block fixed  translate-x-52 bottom-[3rem] left-[23rem] rounded-2xl bg-nav backdrop:blur-20 text-greeen '>
-        <a href="#" className='dactive'><AiOutlineHome/></a>
+      <nav className=' flex  w-fit p-5 gap-6 block fixed  translate-x-52 bottom-[1rem] left-[23rem] rounded-2xl bg-nav backdrop:blur-20 text-greeen '>
+        <a href="#" onClick={()=> setActiveNav('#')} className={activeNav === '#' ? 'active': ''}><AiOutlineHome/></a>
         <a href="#about"><AiOutlineUser/></a>
         <a href="#experience"><BiBook/></a>
         <a href="#services"><RiServerLine/></a>
